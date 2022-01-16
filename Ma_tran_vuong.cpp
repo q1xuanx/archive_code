64be43a104c5d525b6267c1f552609e54796da65
n5an9 <93658293+q1xuanx@users.noreply.github.com>
2022-01-16 15:43:07 +0700

    Update Ma_tran_vuong.cpp

--- Ma_tran_vuong.cpp
+++ Ma_tran_vuong.cpp
@@ -37,17 +37,37 @@ int kiemTraDoiXung (int a[][100], int n){
 	}
 	return 1;
 }
+int tinhTongTrenDuongCheoChinh(int a[][100], int n){
+	int tong = 0;
+	for(int i = 0; i < n - 1; i++){
+		for(int j = i + 1; j < n; j++){
+			tong += a[i][j];
+		}
+	}
+	return tong;
+}
+int tinhTongDuoiDuongCheoChinh(int a[][100], int n){
+	int tong = 0;
+	for(int i = 1; i < n; i++){
+		for(int j = 0; j < i; j++){
+			tong += a[i][j];
+		}
+	}
+	return tong;
+}
 int main(){
 	int n;
 	int a[100][100];
 	taoMang(a,n);
 	xuatMang(a,n);
-	//duongCheoChinh(a,n);
-	//duongCheoPhu(a,n);
+	duongCheoChinh(a,n);
+	duongCheoPhu(a,n);
 	if(kiemTraDoiXung == 0){
-		cout << "Ma tran khong doi xung";
+		cout << "\nMa tran khong doi xung";
 	}else {
-		cout <<"Ma tran doi xung";
+		cout <<"\nMa tran doi xung";
 	}
+	cout << "\nTong cac phan tu tren duong cheo chinh: " << tinhTongTrenDuongCheoChinh(a,n);
+	cout << "\nTong cac phan tu duoi duong cheo chinh: " << tinhTongDuoiDuongCheoChinh(a,n);
 	return 0;
 }

1ca97149d4f313885641b2861757bba7bd2f5948
n5an9 <93658293+q1xuanx@users.noreply.github.com>
2022-01-16 14:49:13 +0700

    Add files via upload

---
+++ Ma_Tran_Vuong.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Ma_Tran_Vuong {
+
+	public Ma_Tran_Vuong() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner (System.in);
+		int n = nhapN(scan);
+		int a[][] = taoMang(n,scan);
+		xuatMang(a,n);
+		duongCheoChinh(a,n);
+		duongCheoPhu(a,n);
+	}
+	public static int nhapN(Scanner scan) {
+		int n = Integer.parseInt(scan.nextLine());
+		return n;
+	}
+	public static int[][] taoMang (int n, Scanner scan){
+		int a[][] = new int[n][n];
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++) {
+				System.out.println("a[" + i +"][" + j +"]: ");
+				a[i][j] = Integer.parseInt(scan.nextLine());
+			}
+		}
+		return a;
+	}
+	public static void xuatMang(int a[][], int n) {
+		for(int i = 0; i < n; i++) {
+			for(int j = 0; j < n; j++) {
+				System.out.print(a[i][j] + "\t");
+			}
+			System.out.println("\n");
+		}
+	}
+	public static void duongCheoChinh(int a[][], int n) {
+		for(int i = 0; i < n; i++) {
+			System.out.print(a[i][i] + "\t");
+		}
+	}
+	public static void duongCheoPhu(int a[][], int n) {
+		for(int i = 0; i < n; i++) {
+			System.out.print(a[i][n-1-i] + "\t");
+		}
+	}
+}

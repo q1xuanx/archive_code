16891696d0c3b40ae3a4944debe44ecf2a713bd2
n5an9 <93658293+q1xuanx@users.noreply.github.com>
2022-07-04 18:36:37 +0700

    Update and rename 1 to Introduce.cpp

--- Introduce.cpp
+++ Introduce.cpp
@@ -1,7 +1,9 @@
+/*
 -Hello my name is Nhan
 -Welcome to my first project !! 
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 -My facebook link: https://www.facebook.com/profile.php?id=100007726748176
 -My instagram link: https://www.instagram.com/qixuanx_/
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-SEE YOU SOON !!! 
+SEE YOU SOON !!!
+*/

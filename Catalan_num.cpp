b41765c82b7af56de9baf56cddcbff9f913009ed
n5an9 <93658293+q1xuanx@users.noreply.github.com>
2022-07-14 14:45:25 +0700

    Create Catalan_num.cpp

---
+++ Catalan_num.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL)
+#define ll long long
+int gt (int n){
+	int s = 1;
+	for (int i = 1;i <= n; i++){
+		s *= i;
+	}
+	return s;
+}
+int soCATALAN (int n){
+	int gt1 = gt(2*n);
+	int gt2 = gt(n+1)*gt(n);
+	return gt1/gt2;
+}
+int main(){
+  fast;
+	int n;
+	cin >> n;
+	cout << "So catalan: " << soCATALAN(n);
+	return 0;
+}
+

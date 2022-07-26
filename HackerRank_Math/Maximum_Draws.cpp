2da2cd85027c87d17b71ecec5ed3e477fcba587b
n5an9 <93658293+q1xuanx@users.noreply.github.com>
2022-07-26 20:31:14 +0700

    Create Maximum_Draws.cpp

---
+++ HackerRank_Math/Maximum_Draws.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(0); cin.tie(0)
+#define ll long long
+const int MAX = 1e5+23;
+void solve (){
+	int n;
+	cin >> n;
+	cout << n + 1 << "\n";
+}	
+int main(){
+	fast;
+	int t; cin >> t;
+	while(t--)
+	solve();
+	return 0;
+}
+

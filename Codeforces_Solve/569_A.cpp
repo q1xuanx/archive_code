73d6c9acdad22fac640dcbcaf6023b6de0cd5511
n5an9 <93658293+q1xuanx@users.noreply.github.com>
2022-08-09 21:53:04 +0700

    Create 569_A.cpp

---
+++ Codeforces_Solve/569_A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(0); cin.tie(0)
+#define ll long long
+#define rep(i,a,b) for (int i = a; i < b;i++)
+void solve (){
+	ll n; cin >> n;
+	cout << 2 * n * n - 2 * n + 1;
+}	
+
+int main(){
+    fast;
+	solve();
+	return 0;
+}

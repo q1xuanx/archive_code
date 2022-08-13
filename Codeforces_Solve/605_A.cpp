5311b0eb089597d327da495d2e963c0783a777ee
n5an9 <93658293+q1xuanx@users.noreply.github.com>
2022-08-13 10:05:48 +0700

    Create 605_A.cpp

---
+++ Codeforces_Solve/605_A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(0); cin.tie(0)
+#define ll long long
+#define rep(i,a,b) for (int i = a; i < b;i++)
+const int inf = 1e9;
+void solve (){
+	ll n; cin >> n;
+	cout << 9 * n << " " << 8 * n;
+}	
+int main(){
+	fast;
+	solve();
+	return 0;
+}
+

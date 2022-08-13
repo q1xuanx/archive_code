e750a88e60f5675917fbde49182e286a015c8fb9
n5an9 <93658293+q1xuanx@users.noreply.github.com>
2022-08-13 20:09:06 +0700

    Create BTR47_A.cpp

---
+++ Codeforces_Solve/BTR47_A.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(0); cin.tie(0)
+#define ll long long
+#define rep(i,a,b) for (int i = a; i < b;i++)
+const int inf = 1e9;
+#define MAX 1000007
+void solve (){
+	int n,m; cin >> n >> m;
+	int area = n * m;
+	cout << area / 2;
+}	
+int main(){
+	fast;
+	solve();
+	return 0;
+}
+

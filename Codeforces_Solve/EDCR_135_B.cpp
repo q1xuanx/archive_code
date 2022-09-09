861f654095a8c94f6564011d9bc1933136931b65
n5an9 <93658293+q1xuanx@users.noreply.github.com>
2022-09-09 08:21:48 +0700

    Create EDCR_135_B.cpp

---
+++ Codeforces_Solve/EDCR_135_B.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/hash_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+#define fast ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+#define ll long long
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define rever(i, a) for (int i = a; i > 0; i--)
+const int inf = 1e9;
+int a[10000001];
+void solve (){
+    int n;
+    cin >> n;
+    if (n % 2 == 0){
+    for (int i = 1; i <= n; i++)
+    {
+        a[i] = i;
+    }
+    for (int i = 1; i <= n - 2; i++)
+    {
+        swap(a[i], a[i + 1]);
+        i += 1;
+    }
+    for (int i = 1; i <= n; i++)
+    {
+        cout << a[i] << " ";
+    }
+    cout << "\n";
+    }else {
+        for (int i = 1; i <= n; i++)
+        {
+            a[i] = i;
+        }
+        for (int i = 2; i <= n - 2; i++)
+        {
+            swap(a[i], a[i + 1]);
+            i += 1;
+        }
+        for (int i = 1; i <= n; i++)
+        {
+            cout << a[i] << " ";
+        }
+        cout << "\n";
+    }
+}
+int main()
+{
+    fast;
+    int t;
+    cin >> t;
+    while (t--){
+        solve();
+    }
+    return 0;
+}

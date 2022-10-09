d4b9a5bc18d32e073c4fee392cc3face2955f74e
n5an9 <93658293+q1xuanx@users.noreply.github.com>
2022-10-09 17:34:02 +0700

    Create Implement strstr.cpp

---
+++ GFG/Implement strstr.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/hash_policy.hpp>
+// YOU CAN DO IT !
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+#define fast ios_base::sync_with_stdio(0), cin.tie(0);
+#define ll long long
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define rever(i, a) for (int i = a; i > 0; i--)
+#define pb push_back
+#define vt vector
+const int inf = 1e9;
+const int MAX = 1000001;
+
+void solve()
+{
+    string s, x;
+    cin >> s >> x;
+    for (int i = 0; i < s.length() - x.length(); i++){
+        int j;
+        for (j = 0; j < x.length(); j++){
+            if (s[i+j] != x[j]) break;
+        }
+        if (j == x.length()){cout << i; return;}
+    }
+    cout << -1;
+}
+int main()
+{
+    fast;
+    int tc = 1;
+    //cin >> tc;
+    while (tc--)
+    {
+        solve();
+    }
+    return 0;
+}
+// 1 2 3 4 5 6
+// a e i o u

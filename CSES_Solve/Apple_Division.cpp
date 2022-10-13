d758993d67fcf759ab3ec01d0e5ea83e330bc071
n5an9 <93658293+q1xuanx@users.noreply.github.com>
2022-10-13 22:23:55 +0700

    Update Apple_Division.cpp

--- CSES_Solve/Apple_Division.cpp
+++ CSES_Solve/Apple_Division.cpp
@@ -1,44 +1,28 @@
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/hash_policy.hpp>
-// YOU CAN DO IT !
 using namespace std;
-using namespace __gnu_pbds;
-typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
-#define fast ios_base::sync_with_stdio(0), cin.tie(0);
-#define ll long long
-#define rep(i, a, b) for (int i = a; i < b; i++)
-#define rever(i, a) for (int i = a; i > 0; i--)
-#define pb push_back
-#define vt vector
-const int inf = 1e9;
-const int MAX = 1000001;
-void solve()
+#define int long long int
+int findminans(int *arr, int currsum, int totalsum, int i)
 {
-    ll int n; 
-    ll int sum = 0, ans; cin >> n;
-    vector<ll int > a(n);
-    for (int i = 0; i < n; i++){
-        cin >> a[i];
-        sum += a[i];
-    }
-    ll int cur = 0;
-    ans = 0;
-    for (int i = 0; i < n; i++){
-        ans = min(cur+a[i], cur);
-    }
-    cout << cur;
+    if (i == 0)
+        return abs((totalsum - currsum) - currsum);
+ 
+    return min(findminans(arr, currsum + arr[i], totalsum, i - 1), findminans(arr, currsum, totalsum, i - 1));
 }
-int main()
+int32_t main()
 {
-    fast;
-    int t = 1;
-    //cin >> t;
-    while (t--)
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int n;
+    cin >> n;
+    int *arr = new int[n];
+    int totalsum = 0;
+    for (int i = 0; i < n; i++)
     {
-        solve();
+        cin >> arr[i];
+        totalsum += arr[i];
     }
+ 
+    cout << findminans(arr, 0, totalsum, n - 1) << endl;
     return 0;
 }
-
-// 11+4 = 15 

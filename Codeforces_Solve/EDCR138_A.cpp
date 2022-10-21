f6c242917712b5ba6055efa53f8154a840dc918b
n5an9 <93658293+q1xuanx@users.noreply.github.com>
2022-10-21 12:14:25 +0700

    Create EDCR138_A.cpp

---
+++ Codeforces_Solve/EDCR138_A.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        int n,m; cin >> n >> m;
+        for (int i = 0; i < m; i++){
+            int x, y; cin >> x >> y;
+        }
+        if (n == m) cout << "NO\n";
+        else cout << "YES\n";
+    }
+    return 0;
+}

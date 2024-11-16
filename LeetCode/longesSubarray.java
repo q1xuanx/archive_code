739e89f2bb95b5ef26d5c1ee0bda642d715bf8c8
n5an9 <93658293+q1xuanx@users.noreply.github.com>
2024-11-16 16:39:00 +0700

    Update longesSubarray.java

--- LeetCode/longesSubarray.java
+++ LeetCode/longesSubarray.java
@@ -2,13 +2,21 @@ class Solution {
     public int longestSubarray(int[] nums) {
         int cnt = 0, j = 0, res = 0; 
         for (int i = 0; i <nums.length; i++){
-            if (nums[i] == 0) cnt++; 
-            while (cnt > 1){
-                if (nums[j] == 0) cnt--;
-                j++; 
+            if (nums[i] == 1) cnt++; 
+            else {
+                j = i + 1;
+                while (j < nums.length){
+                    if (nums[j] == 1){
+                        cnt++; 
+                    }else if (nums[j] == 0) break;
+                    j++;
+                }
+                res = Math.max(res,cnt); 
+                cnt = 0; 
             }
-            res = Math.max(res, i - j);
         }
+        res = Math.max(res,cnt);
+        if (res == nums.length) res -= 1;
         return res; 
     }
 }

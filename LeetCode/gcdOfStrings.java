2c9548133130215fbeb0b4ea5c066592cc720992
n5an9 <93658293+q1xuanx@users.noreply.github.com>
2024-11-29 19:47:47 +0700

    Update gcdOfStrings.java

--- LeetCode/gcdOfStrings.java
+++ LeetCode/gcdOfStrings.java
@@ -1,14 +1,12 @@
 class Solution {
     public String gcdOfStrings(String str1, String str2) {
-        String res = "";
         String temp1 = str1 + str2; 
         String temp2 = str2 + str1; 
         if (temp1.equals(temp2)){
             int l1 = str1.length();
             int l2 = str2.length();
             int gcd = GCD(l1,l2); 
-            if (str1.length() > 0) return str1.substring(0,gcd);
-            else return str2.substring(0,gcd);
+            return str1.substring(0,gcd);
         }
         return "";
     }

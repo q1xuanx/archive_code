aa64a706407bca7aa1cebd50a49e183c4dece386
n5an9 <93658293+q1xuanx@users.noreply.github.com>
2025-01-31 13:41:57 +0700

    Update swapPairs.java

--- LeetCode/swapPairs.java
+++ LeetCode/swapPairs.java
@@ -1,14 +1,18 @@
 class Solution {
     public ListNode swapPairs(ListNode head) {
-        ListNode dummy = new ListNode(0), pre = dummy; 
+        ListNode dummy = new ListNode(0); 
+        // dummy -> 1 -> 2 -> 3 
         dummy.next = head; 
+        ListNode pre = dummy; 
+
         while (pre.next != null && pre.next.next != null){
-            ListNode cur = pre.next, suc = cur.next, tmp = suc.next;
-            pre.next = suc; 
-            suc.next = cur;
-            cur.next = tmp;
-            pre = cur; 
-        }        
-        return dummy.next;
+            ListNode first = pre.next, second = pre.next.next; 
+            first.next = second.next; 
+            second.next = first; 
+            pre.next = second; 
+            pre = first; 
+        }
+
+        return dummy.next; 
     }
 }

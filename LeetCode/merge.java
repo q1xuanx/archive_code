b8088b529333a099738b31a89eb195de19132fca
n5an9 <93658293+q1xuanx@users.noreply.github.com>
2025-03-16 21:21:05 +0700

    Update merge.java

--- LeetCode/merge.java
+++ LeetCode/merge.java
@@ -48,3 +48,23 @@ class Solution {
         return result;  
     }
 }
+// Better Solution
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        List<int[]> result = new ArrayList<>();
+        int l = intervals[0][0];
+        int r = intervals[0][1];
+        for(int i = 1; i < intervals.length; i++){
+            if (l <= intervals[i][0] && intervals[i][0] <= r){
+                r = Math.max(intervals[i][1], r);
+                continue;
+            }
+            result.add(new int []{l,r});
+            l = intervals[i][0];
+            r = intervals[i][1];
+        }
+        result.add(new int[] {l,r});
+        return result.toArray(new int[result.size()][2]);  
+    }
+}
